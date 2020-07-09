70a721124821436ff97ea5e89c4b797fb2b3201d
Francesco Lescai <francesco.lescai@nibsc.org>
2020-07-09 14:22:21 +0100

    basic functions in place

---
+++ R/render.R
@@ -0,0 +1,46 @@
+#' Render NIBSC HTML report
+#'
+#' This function is called in the output of the YAML header of the .Rmd file used to generate the report
+#' to specify the standard output desired as html
+#'
+#' @param ... Arguments to be passed to the function `[bookdown::html_document2]`
+#'
+#' @return A modified `html_document2` with specific parameters for NIBSC reports
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    output: nibscdoc::report_html
+#' }
+#' @import bookdown
+report_html <- function(...){
+  cssfile <- system.file("rmarkdown/templates/analysis-report-html/assets/nibsc.css",
+                                     package="nibscdoc")
+  renderfunction <- bookdown::html_document2(...,
+    css = cssfile,
+    theme = "readable",
+    highlight = "tango",
+    toc = TRUE,
+    toc_float = TRUE
+  )
+
+  ## base settings
+  renderfunction$knitr$opts_chunk$message <- FALSE
+  renderfunction$knitr$opts_chunk$error <- FALSE
+  renderfunction$knitr$opts_chunk$warning <- FALSE
+  renderfunction$knitr$opts_chunk$tidy <- TRUE
+  renderfunction$knitr$opts_chunk$tidy.opts <- list(blank = FALSE)
+  renderfunction$knitr$opts_chunk$cache <- TRUE
+  renderfunction$knitr$opts_chunk$cache.lazy  <-  FALSE
+  renderfunction$knitr$opts_chunk$echo  <-  FALSE
+  renderfunction$knitr$opts_chunk$results  <-  'asis'
+  renderfunction$knitr$opts_chunk$fig.wide <- TRUE
+  renderfunction$knitr$opts_chunk$fig.align <- "center"
+  renderfunction$knitr$opts_chunk$fig.path <- "figures/"
+  renderfunction$knitr$opts_chunk$fig.pos <- "H"
+  renderfunction$knitr$opts_chunk$out.width <- "100%"
+
+  renderfunction
+}
+
+
---
+++ README.md
@@ -0,0 +1,24 @@
+
+# RMarkdown Templates in use at NIBSC
+
+<!-- badges: start -->
+[![R build status](https://github.com/nibscbioinformatics/nibscdoc/workflows/R-CMD-check/badge.svg)](https://github.com/nibscbioinformatics/nibscdoc/actions)
+<!-- badges: end -->
+
+The goal of nibscdoc is to ...
+
+## Installation
+
+The package can be installed from GitHub with:
+
+``` r
+# install.packages("remotes")
+remotes::install_github("nibscbioinformatics/nibscdoc")
+```
+
+## Using from RStudio
+
+The use of any templates is very easy on RStudio, once the package has been installed and RStudio has been restarted.
+
+Just open File > New File > R Markdown > From Template and select the desired template.
+
